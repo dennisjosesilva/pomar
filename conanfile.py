4e71ec75a4e0813c8aa58c762ff079d706280615
Dennis Jose da Silva <dennisjosesilva@gmail.com>
2017-07-14 13:21:42 -0300

    fixing conanfile

--- conanfile.py
+++ conanfile.py
@@ -17,19 +17,19 @@ class PomarConan(ConanFile):
         self.run("cd pomar")
         # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
         # if the packaged project doesn't have variables to set it properly
-        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)", '''PROJECT(MyHello)
+        tools.replace_in_file("pomar/CMakeLists.txt", "PROJECT(pomar)", '''PROJECT(pomar)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
 
     def build(self):
         cmake = CMake(self)
         shared = "-DBUILD_SHARED_LIBS=ON" if self.options.shared else ""
-        self.run('cmake hello %s %s' % (cmake.command_line, shared))
+        self.run('cmake pomar %s %s' % (cmake.command_line, shared))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
         self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*pomar.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
