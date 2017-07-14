b35febfad187f18a8afdd4d08f6ef6bdcddd4e86
Dennis Jose da Silva <dennisjosesilva@gmail.com>
2017-07-14 14:35:31 -0300

    adding test source files.

---
+++ catch/src/AdjacencyRelation/Image2D/Image2DAdjacencyRelationByTranslation.cpp
@@ -0,0 +1,39 @@
+#include "../../../../catch.hpp"
+#include <pomar/AdjacencyRelation/Image2D/Image2DAdjacencyRelationByTranslating.h>
+
+SCENARIO("Image2DAdjacencyRelationByTranslating initialize correctly") {
+  GIVEN("An adjacency relation defined by the translating of the two points: { (-1, 0), (1,0) } and a image size of (3,1)") {
+    int width = 3, height = 1;
+    std::vector<int> dx{-1, 0};
+    std::vector<int> dy{1, 0};
+    Image2DAdjacencyRelationByTranslating adj(width, height, dx, dy);
+
+    WHEN("compute adjacent points of (1,0)") {
+      size_t p = 1;
+      std::vector<int> ap = adj.getAdjacentElements(p);
+      THEN("the adjacent points should be { (0,0), (2,0) }") {
+	REQUIRE(ap[0] == 0);
+	REQUIRE(ap[1] == 2);
+      }
+      THEN("the adjacent points set should contains 2 elements") {
+	REQUIRE(ap.size() == 2);
+      }
+    }
+    WHEN("compute adjacent points of (2,0)") {
+      size_t p = 1;
+      std::vector<int> ap = adj.getAdjacentElements(p);
+      THEN("the adjacent points should be { (1,0), NoAdjacentIndex }") {
+	REQUIRE(ap[0] == 1);
+	REQUIRE(ap[1] == Image2DAdjacencyRelationByTranslating::NoAdjacentIndex);
+      }
+      THEN("the adjacent point set should contains 2 elements") {
+	REQUIRE(ap.size() == 2);
+      }
+    }
+  }
+}
+  
+  /*
+SCENARIO("Image2DAdjacencyRelationByTranslating initialize 8-,4-connectivitily correctly") {
+}
+  */
