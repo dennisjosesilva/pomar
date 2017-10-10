bd7e9a1cd2a9b0a9ae53aadb6e00420ab2ae9803
alexmorimitsu <alexmorimitsu@gmail.com>
2017-10-10 16:24:25 -0300

    Adding getters and setters for Component Graphs

--- include/pomar/ComponentGraph/ComponentGraph.hpp
+++ include/pomar/ComponentGraph/ComponentGraph.hpp
@@ -1,3 +1,6 @@
+#ifndef COMPONENT_GRAPH_H_INCLUDED
+#define COMPONENT_GRAPH_H_INCLUDED
+
 namespace pomar{
   template<class NT>
   class CGNode
@@ -5,6 +8,30 @@ namespace pomar{
   public:
     CGNode();
     CGNode(int id, int adjIndex, NT level);
+
+    inline int id() const { return _id; }
+    inline void id(int id) { _id = id; }
+
+    inline int adjIndex() const { return _adjIndex; }
+    inline void adjIndex(int adjIndex) { _adjIndex = adjIndex; }
+
+    inline const NT& level() const { return _level; }
+    inline void level(const NT& level) { _level = level; }
+
+    inline const std::vector<int>& parents() const { return _parents; }
+    inline void addParent(int parent) { _parents.push_back(parent); }
+
+    inline const std::vector<int>& children() const { return _children; }
+    inline void addChild(int child) { _children.push_back(child); }
+
+    inline const std::vector<int>& composites() const { return _composites; }
+    inline void addComposite(int composite) { _composites.push_back(composite); }
+
+    inline const std::vector<int>& partials() const { return _partials; }
+    inline void addPartial(int partial) { _partials.push_back(partial); }
+
+    inline const std::vector<int>& pixels() const { return _pixels; }
+    inline void addPixel(int pixel) { _pixels.push_back(pixel); }
 
   private:
     int _id;
@@ -32,3 +59,5 @@ namespace pomar{
   /* =================== COMPONENT GRAPH =================================== */
 
 }
+
+#endif
