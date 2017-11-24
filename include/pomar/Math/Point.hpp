b9c0ac4ed7cfe56a3f7c435a849fff308b8e30f7
dennis <dennisjosesilva@gmail.com>
2017-11-24 09:48:52 -0200

    Point2D documentation.

--- include/pomar/Math/Point.hpp
+++ include/pomar/Math/Point.hpp
@@ -3,43 +3,71 @@
 
 namespace pomar
 {
+
+  /**
+  * Class to represent 2D points of any type.
+  */
   template<class T>
   class Point2D
   {
   public:
+    /** Construct a point from 2 values of a type T. */
     Point2D(T px, T py);
 
+    /** Get the value of the coordinate x. */
     inline T x() const { return _x; }
+
+    /** Set a value of the coordinate x. */
     inline void x(int px) { _x = px; }
 
+    /** Get the value of the coordinate y */
     inline T y() const { return _y; }
+
+    /** Get the value of the coordinate y. */
     inline void y(int py) { _y = py; }
 
-
+    /** Sum this point with a point q  using + operator of the type T */
     Point2D<T> sum(const Point2D<T>& q) const;
+
+    /** Subtract point q from a this point using - operator of the type T */
     Point2D<T> sub(const Point2D<T>& q) const;
+
+    /** Check whether this point is equal to a point q or not. */
     bool isEqual(const Point2D<T> &q) const;
-    
+
+    /** Multiplicate this point by a value of type V. */
     template<typename V> Point2D<T> mult(const V& value) const;
+
+    /** Divide this point by a value of type V. */
     template<typename V> Point2D<T> div(const V& value) const;
 
+    /** Shortcut for sum(). */
     Point2D<T> operator+(const Point2D<T> &q) const;
+    /** Update this point by summing it with q. */
     void operator+= (const Point2D<T> &q);
-    
+
+    /** Shortcut for sub().*/
     Point2D operator-(const Point2D<T> &q) const;
+    /** Update this point by subtracting q from it */
     void operator-=(const Point2D<T> &q);
 
+    /** Shortcut for isEqual() */
     bool operator==(const Point2D<T>& q) const;
-    
+
+    /** Shortcut for mult() */
     template<typename V> Point2D<T> operator*(const V& value) const;
+    /** Update this point by multiplying it by value. */
     template<typename V> void operator*=(const V& value);
+    /** Shortcut by div() */
     template<typename V> Point2D<T> operator/(const V& value) const;
+    /** Update this point by dividing it by value. */
     template<typename V> void operator/=(const V& value);
-    
+
   private:
     T _x;
     T _y;
   };
+
 
   using ULPoint2D = Point2D<unsigned long>;
   using LPoint2D = Point2D<long>;
@@ -93,7 +121,7 @@ namespace pomar
     _x /= value;
     _y /= value;
   }
-  
+
   template<class T>
   Point2D<T>::Point2D(T px, T py)
     :_x(px), _y(py)
@@ -116,7 +144,7 @@ namespace pomar
   {
     return _x == q._x && _y == q._y;
   }
-  
+
   template<class T>
   Point2D<T> Point2D<T>::operator+(const Point2D<T> &q) const
   {
