e18c86c844a7beed65d836ab1b5ba7153a912298
dennis <dennisjosesilva@gmail.com>
2017-11-24 09:01:35 -0200

    Remove previous documentation files

--- include/pomar/AdjacencyRelation/Adjacency.hpp
+++ include/pomar/AdjacencyRelation/Adjacency.hpp
@@ -7,10 +7,23 @@
 
 namespace pomar
 {
+  /**
+  * Abstract class which represents an graph adjacency of a set of vertices
+  * stored in a array such with indices from 0 up to the array's length.
+  */
   class Adjacency
   {
   public:
+    /**
+    * Special value to represent a vertex out of set (-1).
+    */
     static const int NoAdjacentIndex;
+
+    /**
+    * It returns the neighbourhood of the vertex identified by id. If
+    * the return has a element out of the vertex set it will be denoted
+    * with value NoAdjacentIndex (-1).
+    */
     virtual const std::vector<int>& neighbours(int id) = 0;
   };
 }
