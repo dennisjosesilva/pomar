6383785ae8c5424db25106a1a1f2d04dfd268cd9
dennis <dennisjosesilva@gmail.com>
2018-02-15 16:35:07 -0200

    implement incremental algorithm to compute area attribute.

---
+++ include/pomar/Attribute/BasicAttributeComputer.hpp
@@ -0,0 +1,88 @@
+#include <pomar/Attribute/AttributeComputer.hpp>
+#include <pomar/Attribute/AttributeCollection.hpp>
+#include <pomar/ComponentTree/CTree.hpp>
+
+#include <memory>
+#include <functional>
+
+#ifndef BASIC_ATTRIBUTE_COMPUTER_HPP_INCLUDED
+#define BASIC_ATTRIBUTE_COMPUTER_HPP_INCLUDED
+
+namespace pomar
+{
+  template<class T>
+  class AreaAttributeComputer
+  {
+  public:
+    AttributeCollection setUp(const CTree<T> &ct);
+    void preProcess(AttributeCollection& attrs, const CTNode<T> &node);
+    void merge(AttributeCollection &attrs, const CTNode<T> &node, 
+      const CTNode<T> &parent);
+    void postProcess(AttributeCollection &attrs, const CTNode<T> &node);
+
+    AttributeCollection compute(const CTree<T> &ct);
+    std::unique_ptr<IncrementalAttributeComputer<T>> toIncrementalAttributeComputer();
+
+  private:
+    int _myIndex;
+  };
+
+  /* ===================== [IMPLEMENTATION ] ================================================= */
+  /* ================ [ AREA ATTRIBUTE COMPUTER ] ============================================ */
+  template<class T>
+  AttributeCollection AreaAttributeComputer<T>::setUp(const CTree<T> &ct)
+  { 
+    AttributeCollection attrs;
+    attrs.push(AttrType::AREA, ct.numberOfNodes());
+    _myIndex = attrs.attrIndex(AttrType::AREA);
+    return attrs;
+  }
+
+  template<class T>
+  void AreaAttributeComputer<T>::preProcess(AttributeCollection& attrs, 
+    const CTNode<T> &node)
+  {
+    attrs[_myIndex][node.id()] += node.elementIndices().size();
+  }
+
+  template<class T>
+  void AreaAttributeComputer<T>::merge(AttributeCollection &attrs, const CTNode<T> &node, 
+    const CTNode<T> &parent)
+  {
+    attrs[_myIndex][parent.id()] += attrs[_myIndex][node.id()];
+  }
+
+  template<class T>
+  void AreaAttributeComputer<T>::postProcess(AttributeCollection &attrs, const CTNode<T> &node)
+  {
+    /*This method is kept empty. */
+  }
+
+  template<class T>
+  AttributeCollection AreaAttributeComputer<T>::compute(const CTree<T> &ct)
+  {
+    auto computer = toIncrementalAttributeComputer();
+    return computer->doCompute(ct);
+  }
+
+  template<class T>
+  std::unique_ptr<IncrementalAttributeComputer<T>> 
+    AreaAttributeComputer<T>::toIncrementalAttributeComputer()
+  {
+    auto mySetup = [this](const CTree<T> &ct) { return this->setUp(ct); };
+    auto myPreProcess = [this](AttributeCollection& attrs, const CTNode<T> &node) {
+      this->preProcess(attrs, node);
+    };
+    auto myMerge = [this](AttributeCollection &attrs, const CTNode<T> &node, 
+      const CTNode<T> &parent) {
+      this->merge(attrs, node, parent);
+    };
+    auto myPostProcess = [this](AttributeCollection &attrs, const CTNode<T> &node) {
+      this->postProcess(attrs, node);
+    };
+
+    return std::unique_ptr<IncrementalAttributeComputer<T>>(
+      new IncrementalAttributeComputer<T>(myPreProcess, myMerge, myPostProcess, mySetup));
+  }
+}
+#endif  
